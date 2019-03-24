cda0a5b41da7d0839b791be6895d4e82c98240d6
sandwichdoge <sandwichdoge@gmail.com>
2019-03-24 13:44:13 +0700

    unify unit test format

--- ini/build.sh
+++ ini/build.sh
@@ -1,2 +1,2 @@
-g++ unit_test/test_ini_main.cpp ini.cpp  ../gtestlib/libgtest.a  -o run_tests.out -I../gtestlib/ -pthread
+g++ unit_test/test_main.cpp ini.cpp  ../gtestlib/libgtest.a  -o run_tests.out -I../gtestlib/ -pthread
 ./run_tests.out && rm run_tests.out
