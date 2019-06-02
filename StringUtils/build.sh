316f129c7137a6dc1f8a53fc9a1a96282c0ae8cc
Thuan Ho <sandwichdoge@gmail.com>
2019-06-02 21:50:27 +0700

    WStringUtils, fix StringUtils

--- StringUtils/build.sh
+++ StringUtils/build.sh
@@ -1,7 +1,5 @@
-g++ -c StringUtils.cpp -O3 -fPIC -march=native -static -o StringUtils.o
-
-ld -r StringUtils.o -o libStringUtils.a
+g++ -c StringUtils.cpp -O3 -fPIC -march=native -static -o libStringUtils.a
 
 # Unit test
-g++ unit_test/test_main.cpp -lStringUtils ../gtestlib/libgtest.a  -o run_tests.out -I../gtestlib/ -pthread
+g++ unit_test/test_main.cpp libStringUtils.a ../gtestlib/libgtest.a  -o run_tests.out -I../gtestlib/ -pthread
 ./run_tests.out && rm run_tests.out
