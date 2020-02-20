7852c0896de96db0e0c748eb34506965506df611
Thuan Ho <sandwichdoge@gmail.com>
2020-02-20 18:03:10 +0700

    StringUtils now header-only

--- StringUtils/StringUtils.h
+++ StringUtils/StringUtils.h
@@ -9,19 +9,96 @@
 
 class StringUtils
 {
-    public:
-        StringUtils();
-        virtual ~StringUtils();
+public:
+    StringUtils();
+    virtual ~StringUtils();
 
-        // Get a list of strings between brackets c
-        static bool betweenBrackets(std::vector<std::string> &out, std::string str, char c);
+    // Get a list of strings between brackets c
+    static bool betweenBrackets(std::vector<std::string> &out, std::string str, char c) {
+        int isInsideBrackets = 0;
+        size_t start = 0;
+        size_t end = 0;
+
+        std::string s = "";
+        out.clear();
+
+        for (size_t i = 0; i < str.length(); i++) {
+            if (str[i] == c) {
+                isInsideBrackets ^= 1;
+
+                if (isInsideBrackets) {
+                    start = i;
+                }
+                else { // Closing bracket
+                    end = i;
+                    if (end > start + 1) { // ""
+                        s = str.substr(start + 1, end - start - 1);
+                    } else {
+                        s = "";
+                    }
+                
+                    out.push_back(s);
+                }
+            }
         
-        // Count number of chars c in string
-        static int charCount(std::string str, char c);
+        }
 
-        static bool stringTrimLeft(std::string &s, size_t n);
+        return true;
+    }
+    
+    // Count number of chars c in string
+    static int charCount(std::string str, char c) {
+        int ret = 0;
 
-        static bool stringTrimRight(std::string &s, size_t n);
+        for (size_t i = 0; i < str.length(); i++) {
+            if (str[i] == c) {
+                ret++;
+            }
+        }
 
-        static bool stringReplace(std::string &s, std::string sub_old, std::string sub_new);
+        return ret;
+    }
+
+    static bool stringTrimLeft(std::string &s, size_t n) {
+        if (s.length() == 0 || n < 0) return false;
+
+        if (n >= s.length()) {
+            s = "";
+            return true;
+        }
+
+        s = s.substr(n, s.length() - 1);
+
+        return true;
+    }
+
+    static bool stringTrimRight(std::string &s, size_t n) {
+        #define ZERO_FLOOR(n) (n < 0 ? n == 0 : n)
+        if (s.length() == 0 || n < 0) return false;
+
+        if (n >= s.length()) {
+            s = "";
+            return true;
+        }
+
+        s = s.substr(0, ZERO_FLOOR(s.length() - n));
+
+        return true;
+    }
+
+    static bool stringReplace(std::string &s, std::string sub_old, std::string sub_new) {
+        if (sub_old == sub_new || sub_old.empty()) return false;
+
+        size_t old_pos = 0;
+    
+        while (1) {
+            old_pos = s.find(sub_old, old_pos);
+            if (old_pos == std::string::npos) break;
+
+            s.replace(old_pos, sub_old.length(), sub_new);
+            old_pos += sub_new.length();
+        }
+
+        return true;
+    }
 };
--- StringUtils/build.sh
+++ StringUtils/build.sh
@@ -1,5 +1,3 @@
-g++ -c StringUtils.cpp -O3 -fPIC -march=native -static -o libStringUtils.a
-
 # Unit test
-g++ unit_test/test_main.cpp libStringUtils.a ../gtestlib/libgtest.a  -o run_tests.out -I../gtestlib/ -pthread
+g++ unit_test/test_main.cpp ../gtestlib/libgtest.a  -o run_tests.out -I../gtestlib/ -pthread
 ./run_tests.out && rm run_tests.out
