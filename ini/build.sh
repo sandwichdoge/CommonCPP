4c43087d2face3582803a2b30e687ce3354cf064
Thuan Ho <sandwichdoge@gmail.com>
2020-02-20 18:06:34 +0700

    ini now header-only

--- ini/build.sh
+++ ini/build.sh
@@ -1,5 +1,3 @@
-g++ -c ini.cpp -O3 -fPIC -march=native -o libini.so
-
 # Unit test
-g++ unit_test/test_main.cpp libini.so  ../gtestlib/libgtest.a  -o run_tests.out -I../gtestlib/ -pthread
+g++ unit_test/test_main.cpp  ../gtestlib/libgtest.a  -o run_tests.out -I../gtestlib/ -pthread
 ./run_tests.out && rm run_tests.out
